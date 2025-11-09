2a0766b5637477f31d4d8c1dd7e0242495e0afd6
aiinuuazzariaa <ainu.azzaria@gmail.com>
2025-11-09 22:17:42 +0700

    upd: update text and button sign in

--- resources/views/pages/sign-in.blade.php
+++ resources/views/pages/sign-in.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <main class="main-content  mt-0">
+    <main class="main-content mt-0">
         <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg"
-            style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signup-cover.jpg'); background-position: top;">
+            style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signup-cover.jpg');
+            background-attachment: fixed;">
             <span class="mask bg-gradient-dark opacity-6"></span>
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-lg-5 text-center mx-auto">
                         <h1 class="text-white mb-2 mt-5">Welcome!</h1>
-                        <p class="text-lead text-white">Use these awesome forms to login or create new account in your
-                            project for free.</p>
+                        <p class="text-lead text-white">Keep up the good work and have a good day!</p>
                     </div>
                 </div>
             </div>
@@ -34,10 +34,8 @@
                                         aria-label="Password">
                                 </div>
                                 <div class="text-center">
-                                    <button type="submit" class="btn bg-gradient-dark w-100 my-4 mb-2">Sign in</button>
+                                    <button type="submit" class="btn bg-gradient-primary w-100 my-4 mb-2">Sign in</button>
                                 </div>
-                                {{-- <p class="text-sm mt-3 mb-0">Already have an account? <a href="javascript:;"
-                                        class="text-dark font-weight-bolder">Sign in</a></p> --}}
                             </form>
                         </div>
                     </div>
